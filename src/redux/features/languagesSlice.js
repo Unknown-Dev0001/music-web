d88e9d1de782979d73154afd90828891c0fcac9f
Unknown Developer <138309645+Unknown-Dev0001@users.noreply.github.com>
2025-05-30 18:36:38 +0530

    Update languagesSlice.js

--- src/redux/features/languagesSlice.js
+++ src/redux/features/languagesSlice.js
@@ -1,7 +1,7 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-    languages:  ['hindi', 'punjabi'],
+    languages:  ['hindi', 'english'],
 };
 
 const languagesSlice = createSlice({
